a8bbd236d5c14ae4318733f9204bf553f2e02db2
Deepam Kapur <deepamkapur@gmail.com>
2021-05-23 22:41:22 +0530

    basic operations

--- js/gameController.js
+++ js/gameController.js
@@ -1,4 +1,43 @@
 var app = angular.module('ticTacToeApp', []);
 app.controller('tieTacToeController', function ($scope) {
-    
+    $scope.game = [];
+    $scope.isStart = false;
+    var emptyRow = [{
+        cellValue: ""
+    }, {
+        cellValue: ""
+    }, {
+        cellValue: ""
+    }];
+    var circle = "circle-svg";
+    var cross = "cross-svg";
+
+    $scope.isLeftTurn = true;
+    $scope.leftValue = circle;
+    $scope.rightValue = cross;
+
+    $scope.resetGame = function () {
+        $scope.isStart = false;
+        $scope.game = [];
+        $scope.game.push(angular.copy(emptyRow));
+        $scope.game.push(angular.copy(emptyRow));
+        $scope.game.push(angular.copy(emptyRow));
+    };
+    $scope.resetGame();
+
+    $scope.addCircleOrCross = function (ele) {
+        if (ele.cellValue.trim() == "") {
+            if ($scope.isLeftTurn) {
+                ele.cellValue = $scope.leftValue;
+            }
+            else {
+                ele.cellValue = $scope.rightValue;
+            }
+            $scope.isLeftTurn = !$scope.isLeftTurn;
+        }
+    };
+
+    function checkResult(){
+        
+    }
 });
